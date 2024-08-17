0859023da07044a1d2f860f33375b148174fee3e
Aditya <2002adityaprakash@gmail.com>
2024-08-17 23:19:33 +0530

    1st push

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import booksRoute from './routes/booksRoute.js';
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json()); // Parse incoming requests data as JSON
+
+//middleware for handling cor policys
+app.use(cors());//allow all origins with default cors
+// app.use(cors({
+//     origin: "http://localhost:5555",
+//     method: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+
+// }
+// ))
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial');
+});
+
+app.use('/books', booksRoute);
+
+mongoose.connect(mongoDBURL).then(() => {
+    console.log("Database is connected");
+    app.listen(PORT, () => {
+        console.log(`App is listening on port ${PORT}`);
+    });
+}).catch((error) => {
+    console.log("Error connecting to database:", error);
+});
