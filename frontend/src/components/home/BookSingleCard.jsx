5a5c220affc030ada17d222eccd07aa9b2f26de8
Aditya <2002adityaprakash@gmail.com>
2024-08-18 00:43:29 +0530

    fixed card ui

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -1,7 +1,7 @@
 
 import { AiOutlineEdit } from "react-icons/ai";
 import { BiUserCircle, BiShow } from "react-icons/bi";
-import { BsInfoCircle, BsFillQuestionSquareFill } from "react-icons/bs";
+import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { PiBookOpenTextLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
@@ -28,10 +28,7 @@ const BookSingleCard = ({ book }) => {
         <BiUserCircle className="text-red-300 text-2xl" />
         <h2 className="my-1">{book.author}</h2>
       </div>
-      <div className="flex justify-center items-center gap-x-2">
-        <BsFillQuestionSquareFill className="text-red-300 text-2xl" />
-        <h2 className="my-1">{book.about}</h2>
-      </div>
+     
       <div className="flex justify-center items-center gap-x-2 mt-4 p-4">
         <BiShow
           className="text-3xl text-blue-800 hover:text-black cursor-pointer"
